99a4d0949ed11a2490c3d6eac44c9ed2c0b52512
npezzotti <npezzotti@hotmail.com>
2020-01-29 23:31:29 -0500

    changed signup to sign up

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -70,7 +70,7 @@ export default class Signup extends Component {
         const { name, email, password, error, open } = this.state;
         return (
             <div className="container">
-                <h2 className="mt-5 mb-5">Signup</h2>
+                <h2 className="mt-5 mb-5">Sign up</h2>
                 <div className="alert alert-danger" style={{ display: error ? "" : "none" }}>
                     {error}
                 </div>
