a9d2894763646ad6ab1443d2c34636af01ad40f3
npezzotti <nathan.pezzotti@datadoghq.com>
2020-06-20 16:36:05 -0400

    added confirm password to signup form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -9,6 +9,7 @@ export default class Signup extends Component {
             name: "",
             email: "",
             password: "",
+            password2: "",
             error: "",
             open: false
         }
@@ -23,7 +24,8 @@ export default class Signup extends Component {
 
     onSubmit = event => {
         event.preventDefault()
-        const { name, email, password } = this.state
+        const { name, email, password, password2 } = this.state
+        if (password !== password2) return this.setState({ error: "Passwords must match" });
         const user = {
             name,
             email,
@@ -39,6 +41,7 @@ export default class Signup extends Component {
                     name: "",
                     email: "",
                     password: "",
+                    password2: "",
                     error: "",
                     open: true
                 })
@@ -46,7 +49,7 @@ export default class Signup extends Component {
         })
     }
 
-    signupForm = (name, email, password) => (
+    signupForm = (name, email, password, password2) => (
         <form>
             <div className="form-group">
                 <label className="text-muted">Name</label>
@@ -60,6 +63,10 @@ export default class Signup extends Component {
                 <label className="text-muted">Password</label>
                 <input onChange={this.handleChange("password")} value={password} type="password" className="form-control" />
             </div>
+            <div className="form-group">
+                <label className="text-muted">Confirm Password</label>
+                <input onChange={this.handleChange("password2")} value={password2} type="password" className="form-control" />
+            </div>
             <button onClick={this.onSubmit} className="btn btn-raised btn-primary">
                 Submit
             </button>
@@ -67,7 +74,7 @@ export default class Signup extends Component {
     )
 
     render() {
-        const { name, email, password, error, open } = this.state;
+        const { name, email, password, password2, error, open } = this.state;
         return (
             <div className="container">
                 <h2 className="mt-5">Sign up</h2>
@@ -78,7 +85,7 @@ export default class Signup extends Component {
                 <div className="alert alert-primary" style={{ display: open ? "" : "none" }}>
                     New account successfully created- please <Link to="/signin">sign in.</Link>
                 </div>
-                {this.signupForm(name, email, password)}
+                {this.signupForm(name, email, password, password2)}
             </div>
         )
     }
