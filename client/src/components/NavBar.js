c7b5c9861cf42dad4ea8265f09406ca52a4e0882
npezzotti <nathan.pezzotti@datadoghq.com>
2020-06-26 22:43:38 -0400

    changed conditional rendering on navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -15,14 +15,14 @@ function NavBar({ history }) {
             </button>
             <div className="collapse navbar-collapse" id="collapsibleNavbar">
             <ul className="navbar-nav">
-                <li className="navbar-item">
-                    <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                    <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>              
                 {isAuthenticated() && (
                     <>
+                        <li className="navbar-item">
+                            <Link to="/" className="nav-link">Todos</Link>
+                        </li>
+                        <li className="navbar-item">
+                            <Link to="/create" className="nav-link">Create Todo</Link>
+                        </li>              
                         <li className="navbar-item">
                             <Link to='/settings' className="nav-link">Settings</Link>
                         </li>
